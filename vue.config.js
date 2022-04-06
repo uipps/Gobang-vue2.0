51f14eac8adf9328142425f72b9c987295f6dbbf
tom-cf <uipps_com@163.com>
2022-04-07 01:00:31 +0800

    修改配置后，yarn run build 得到的，http://127.0.0.1:20011/能正常打开

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 
 module.exports = {
   // 基本路径
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Gobang-vue2.0/dist/'
-    : '/',
+  //publicPath: process.env.NODE_ENV === 'production'
+  //  ? '/Gobang-vue2.0/dist/'
+  //  : '/',
   // 生产环境是否生成 sourceMap 文件
   productionSourceMap: false,
   // 服务器端口号
   devServer: {
-    port: 8080,
+    port: 8081,
   },
 }
